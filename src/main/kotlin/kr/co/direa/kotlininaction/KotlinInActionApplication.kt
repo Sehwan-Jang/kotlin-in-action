7b7890b769786ed1d61da2c4635579c6924d9644
sehwan.jang <whilter08@direa.co.kr>
2025-06-20 16:33:06 +0900

    first commit

---
+++ src/main/kotlin/kr/co/direa/kotlininaction/KotlinInActionApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.direa.kotlininaction
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinInActionApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinInActionApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/direa/kotlininaction/KotlinInActionApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.co.direa.kotlininaction
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinInActionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
