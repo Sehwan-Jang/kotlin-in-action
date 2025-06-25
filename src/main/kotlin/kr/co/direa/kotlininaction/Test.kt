701cfffd2933e1aba14fda0fae95f8011989b08b
sehwan.jang <whilter08@direa.co.kr>
2025-06-25 10:55:41 +0900

    test controller

---
+++ src/main/kotlin/kr/co/direa/kotlininaction/Test.kt
@@ -0,0 +1,25 @@
+package kr.co.direa.kotlininaction
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import kotlin.coroutines.coroutineContext
+
+@RestController
+class TestController {
+
+    @GetMapping("/test")
+    fun test(): List<Test> {
+        val tests = mutableListOf<Test>()
+        val names = listOf("a", "b", "c")
+        repeat(10) {
+            val age = (1..100).random()
+            tests.add(Test(name = names.random(), age = age))
+        }
+        return tests
+    }
+}
+
+data class Test(
+        val name: String,
+        var age: Int? = null
+)
